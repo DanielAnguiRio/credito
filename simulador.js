e02aca6e098ff806b6c46c8012ec890587168a9d
DanielAnguiRio <anguisacaandres0106@gmail.com>
2026-04-20 16:54:01 -0500

    validaciones creadas

--- simulador.js
+++ simulador.js
@@ -1,4 +1,74 @@
+function validarFormulario(){
+
+    let valido = true;
+
+    valido &= validarCampo("txtIngresos","errIngresos",1,99999,false);
+    valido &= validarCampo("txtEgresos","errEgresos",0,99999,true);
+    valido &= validarCampo("txtMonto","errMonto",1,99999,false);
+    valido &= validarCampo("txtPlazo","errPlazo",1,30,false);
+    valido &= validarCampo("txtTasaInteres","errTasa",1,100,false);
+
+    return Boolean(valido);
+}
+
+function validarCampo(idInput, idError, min, max, permiteCero){
+
+    let input = document.getElementById(idInput);
+    let error = document.getElementById(idError);
+    let valor = input.value.trim();
+
+    input.classList.remove("input-error","input-ok");
+
+    if(valor === ""){
+        error.innerText = "Campo obligatorio";
+        input.classList.add("input-error");
+        return false;
+    }
+
+    if(isNaN(valor)){
+        error.innerText = "Solo números";
+        input.classList.add("input-error");
+        return false;
+    }
+
+    if(valor.length > 5){
+        error.innerText = "Máximo 5 dígitos";
+        input.classList.add("input-error");
+        return false;
+    }
+
+    let num = parseFloat(valor);
+
+    if(!permiteCero && num <= 0){
+        error.innerText = "Debe ser mayor a 0";
+        input.classList.add("input-error");
+        return false;
+    }
+
+    if(permiteCero && num < 0){
+        error.innerText = "No puede ser negativo";
+        input.classList.add("input-error");
+        return false;
+    }
+
+    if(num < min || num > max){
+        error.innerText = `Rango permitido: ${min} - ${max}`;
+        input.classList.add("input-error");
+        return false;
+    }
+
+    error.innerText = "";
+    input.classList.add("input-ok");
+
+    return true;
+}
+
 function calcular(){
+
+    if(!validarFormulario()){
+        return;
+    }
+
     let ingresos = recuperarFloat("txtIngresos");
     let egresos = recuperarFloat("txtEgresos");
     
